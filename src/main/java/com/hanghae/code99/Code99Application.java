72d7815cd0c506c613218441c4a01ea2d5b61aee
Puri12 <tkdqja9812@gmail.com>
2022-10-28 14:48:07 +0900

    [init] First Commit

---
+++ src/main/java/com/hanghae/code99/Code99Application.java
@@ -0,0 +1,13 @@
+package com.hanghae.code99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code99Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Code99Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/code99/Code99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.code99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code99ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
