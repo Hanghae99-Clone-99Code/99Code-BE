ac4dae67d0c974059a6c5709e2e2d4be742d49d3
Puri12 <tkdqja9812@gmail.com>
2022-11-02 16:14:46 +0900

    fix : 누락된 항목 추가(2)

--- src/main/java/com/hanghae/code99/domain/Member.java
+++ src/main/java/com/hanghae/code99/domain/Member.java
@@ -58,4 +58,8 @@ public class Member extends Timestamped {
         this.introduce = requestDto.getIntroduce();
     }
 
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
 }
