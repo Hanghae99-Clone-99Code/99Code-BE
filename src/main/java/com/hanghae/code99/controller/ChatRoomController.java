ad25983f75abd62565270b6afbe9f81b7dbba2dc
Puri12 <tkdqja9812@gmail.com>
2022-11-02 23:54:48 +0900

    feat : Stomp 메세지 형식 변경

--- src/main/java/com/hanghae/code99/controller/ChatRoomController.java
+++ src/main/java/com/hanghae/code99/controller/ChatRoomController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/chat")
 public class ChatRoomController {
     private final ChatRoomService chatService;
 
