c606b4a6d9309e8c0a8fddf9eda0cb24ddc10725
Puri12 <tkdqja9812@gmail.com>
2022-11-03 00:37:02 +0900

    fix : 오류수정

--- src/main/java/com/hanghae/code99/service/KafkaConsumerService.java
+++ src/main/java/com/hanghae/code99/service/KafkaConsumerService.java
@@ -1,6 +1,7 @@
 package com.hanghae.code99.service;
 
 import com.hanghae.code99.controller.response.ChatMessageDto;
+import com.hanghae.code99.domain.message.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -13,14 +14,10 @@ public class KafkaConsumerService {
     private final SimpMessageSendingOperations sendingOperations;
 
     @KafkaListener(topics = "code99", groupId = "code99", containerFactory = "kafkaListener")
-    public void consume(ChatMessageDto message) {
-        System.out.println("카프카 컨슈머 = " + message.getMessage());
-
-        if (ChatMessageDto.MessageType.ENTER.equals(message.getType())) {
-            message.setMessage(message.getSender()+"님이 입장하였습니다.");
-        }
-
-        sendingOperations.convertAndSend("/topic/chat/room/"+message.getRoomId(), message );
+    public void consume(ChatMessage message) {
+        System.out.println("카프카 컨슈머 = " + message.getMember().getNickname());
+        System.out.println(message);
+        sendingOperations.convertAndSend("/topic/chat/room/"+message.getRoom().getRoomId(), message );
     }
 }
 
